d96a5cc24c760de161964bf304ce8745420ea61e
BirutescoAdicional <137656201+BirutescoAdicional@users.noreply.github.com>
2023-10-07 10:54:43 -0300

    Avanço nos estudos do curso de JavaScript

---
+++ Aula04/ex001.js
@@ -0,0 +1,7 @@
+var nota = 9.0
+console.log(`Você tirou ${nota} no exame...`)
+if (nota >= 6) {
+    console.log('Parabéns! Você passou!')
+} else (nota < 6); {
+    console.log(`Você falhou, estude mais na próxima!`)
+}
